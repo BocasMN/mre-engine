f5c787e4bb8f672ee9ae31ab69e069e93e22bf37
BocasMN <Ricardonnoeiras@gmail.com>
2026-01-31 17:06:43 +0100

    Add files via upload

---
+++ analyze.mjs
@@ -0,0 +1,1 @@
+// analyze function placeholder
--- sw.js
+++ sw.js
@@ -1,18 +1,1 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('mre-cache-v1').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './app.js',
-      './manifest.json'
-    ]))
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+// service worker v3 placeholder
